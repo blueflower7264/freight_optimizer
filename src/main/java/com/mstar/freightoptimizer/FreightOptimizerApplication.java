ada570fd5930b2e0ef2992b28e1b3bead557c3aa
blueflower7264 <blueflower7264@outlook.com>
2025-09-29 11:30:37 -0400

    add files

---
+++ src/main/java/com/mstar/freightoptimizer/FreightOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.mstar.freightoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreightOptimizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mstar/freightoptimizer/FreightOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstar.freightoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightOptimizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
